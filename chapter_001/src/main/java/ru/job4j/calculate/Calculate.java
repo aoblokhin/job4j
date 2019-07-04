35668cedb788ebf151df591d56cc3aaa367865d5
Aex Blokhin <ao.blokhin@ya.ru>
2019-07-04 18:59:16 +0300

    1.3. JavaDoc. Code convention[#154085] - поправлено

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,15 @@
 package ru.job4j.calculate;
 /**
+ * Calculate
+ *
  * The construction - string output in console
- * @param arg - arg.
+ * @author Alex Blokhin 
  */
 public class Calculate {
+	/**
+	 * Main method.
+	 * @param args - args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello, Job4j.");
 	}
